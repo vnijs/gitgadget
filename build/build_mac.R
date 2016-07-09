ad104fd9625ce157dade01bc9b29a2190bc102d0
vnijs <radiant@rady.ucsd.edu>
2016-07-09 02:43:43 -0700

    build R package

---
+++ build/build_mac.R
@@ -0,0 +1,8 @@
+## build for mac
+app <- "gitgadget"
+path <- "~/gh"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
---
+++ build/build_win.R
@@ -0,0 +1,9 @@
+## build for windows
+app <- "gitgadget"
+path <- "../"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
+
