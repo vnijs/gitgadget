899797278d376584db8df74e85e2a3cb48a90c81
vnijs <radiant@rady.ucsd.edu>
2017-05-16 19:03:29 -0700

    push and pull

--- build/build_win.R
+++ build/build_win.R
@@ -1,4 +1,12 @@
 ## build for windows
+rv <- R.Version()
+rv <- paste0(rv$major,".", strsplit(rv$minor,".", fixed = TRUE)[[1]][1])
+
+rvprompt <- readline(prompt = paste0("Running for R version: ", rv, ". Is that what you wanted y/n: "))
+if (grepl("[nN]", rvprompt))
+  stop("Change R-version using Rstudio > Tools > Global Options > Rversion")
+
+## build for windows
 app <- "gitgadget"
 path <- "../"
 devtools::install(file.path(path, app))
