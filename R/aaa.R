d2ec0e565b3b259ae4f271d0a54f2ca31acda7bf
vnijs <radiant@rady.ucsd.edu>
2018-07-15 15:21:00 -0700

    update

--- R/aaa.R
+++ R/aaa.R
@@ -7,9 +7,10 @@ globalVariables(c(".", "un", "directory", "base64_enc", "server"))
 #' @docType package
 #' @import shiny miniUI curl dplyr
 #' @importFrom jsonlite fromJSON
-#' @importFrom rstudioapi isAvailable getActiveProject selectFile selectDirectory openProject
+#' @importFrom rstudioapi isAvailable getActiveProject openProject
 #' @importFrom markdown markdownToHTML
 #' @importFrom utils read.csv capture.output
 #' @importFrom stats na.omit setNames
 #' @importFrom utils packageVersion browseURL
+#' @importFrom usethis edit_r_environ edit_r_profile
 NULL
