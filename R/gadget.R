6b9ef2a782b9b4b16498e3a4181f8784b2804d45
vnijs <radiant@rady.ucsd.edu>
2018-07-15 22:07:59 -0700

    docker paths

--- R/gadget.R
+++ R/gadget.R
@@ -48,6 +48,21 @@ gitgadget <- function(port = get_port()) {
   gg_volumes <- c(Home = homedir)
   git_home <- Sys.getenv("git.home")
 
+  ## needed if using gitgadget with both native and docker version of Rstudio
+  if (git_home != "" && !file.exists(git_home)) {
+    git_alt <- file.path(homedir, basename(git_home))
+    if (file.exists(git_alt)) {
+      git_home <- git_alt
+    } else {
+      git_alt <- file.path(homedir, basename(dirname(git_home)), basename(git_home))
+      if (file.exists(git_alt)) {
+        git_home <- git_alt
+      } else {
+        git_home <- ""
+      }
+    }
+  }
+
   ## setting up volumes for shinyFiles
   if (git_home != "") {
     gg_volumes <- setNames(c(git_home, gg_volumes), c(basename(git_home), names(gg_volumes)))
